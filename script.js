776ce4624db01786be552df7bfe48dfddd82c4bc
yuleisyastrid <yuleisyastrid123@gmail.com>
2019-07-11 20:34:14 -0600

    fin

---
+++ script.js
@@ -0,0 +1,41 @@
+var myWeb = angular.module("myWeb",[]);
+myWeb.controller("Cartoon",function($scope){
+	$scope.personaje = [{
+      nombre: "Ed , Edd y Eddy",
+      img: "1.jpg"
+    }, {
+      nombre: "La Pantera Rosa",
+      img: "2.jpg"
+    }, {
+      nombre: "Box buny",
+      img: "3.jpg"   
+    },
+     {
+      nombre: "Dexter",
+      img: "4.jpg"   
+    },
+     {
+      nombre: "La Muerte",
+      img: "5.jpg"   
+    },
+     {
+      nombre: "Coraje",
+      img: "img/6.jpg"   
+    },
+     {
+      nombre: "Jhony Bravo",
+      img: "7.jpg"   
+    },
+     {
+      nombre: "Bloo",
+      img: "8.jpg"   
+    },
+     {
+      nombre: "Coco Coco",
+      img: "9.jpg"   
+    },
+     {
+      nombre: "Rad Rad",
+      img: "10.jpg"   
+    },];
+})
